6e906617ac2cff6482c59b6f6eef39fd127dc751
Даниил Русанов <Trololomor@mail.ru>
2024-10-10 18:07:45 +0800

    Добавлена вариативность тестов

--- Praktice_Kustov/Praktice_Kustov.cpp
+++ Praktice_Kustov/Praktice_Kustov.cpp
@@ -41,24 +41,45 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
    
-    //тест использования функции
-    int N = 5; //размер матрицы
-    vector<vector<int>> image = {
-        {10, 20, 30, 40, 50},
-        {60, 70, 80, 90, 100},
-        {110, 120, 130, 140, 150},
-        {160, 170, 180, 190, 200},
-        {210, 220, 230, 240, 250},
-    };
+    int vvod = 0;
 
-    vector<vector<int>> blurredImage = blurImage(image);
-
-    //вывод размытого изображения
-    for (const auto& row : blurredImage) {
-        for (int pixel : row) {
-            cout << pixel << " ";
-        }
+    while (true) {
+        cout << "ЗАДАНИЕ: Создание функции размытия изображения" << endl;
         cout << endl;
+        cout << "1. Автоматическая проверка" << endl;
+        cout << "2. Ручаная проверка" << endl;
+        cout << "Введите пункт, как провести проверку теста: ";
+        cin >> vvod;
+
+        if (vvod == 1) { //тест использования функции АВТОМАТИЧЕСКИЙ
+            
+            int N = 5; //размер матрицы
+            vector<vector<int>> image = {
+                {10, 20, 30, 40, 50},
+                {60, 70, 80, 90, 100},
+                {110, 120, 130, 140, 150},
+                {160, 170, 180, 190, 200},
+                {210, 220, 230, 240, 250},
+            };
+
+            vector<vector<int>> blurredImage = blurImage(image);
+
+            //вывод размытого изображения
+            for (const auto& row : blurredImage) {
+                for (int pixel : row) {
+                    cout << pixel << " ";
+                }
+                cout << endl;
+
+            }
+            cout << endl;
+        }
+        if (vvod == 2) { //тест использования функции РУЧНОЙ
+            cout << "Ручная проверка" << endl;
+            cout << endl;
+        }
+
+
     }
 }
 
